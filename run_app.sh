60fa87a0d4680ca77c6e02af355f278c13b2bc5e
Tom Martin <tmartin14@gmail.com>
2019-07-16 15:09:25 -0400

    Updated run_app.sh to enable easy switching of backends
    Updated run_app.sh to enable easy switching of backends between Splunk and Zipkin backend servers via environment variables.

--- run_app.sh
+++ run_app.sh
@@ -1,5 +1,12 @@
+# Use this to send traces to Zipkin backend
+#export RECORDER_URL=http://localhost:9411/api/v2/spans
+#export RECORDER_AUTH=''
+
+# Use this to send traces to Splunk backend
 export RECORDER_URL=http://localhost:8088/services/collector/raw
 export RECORDER_AUTH='Splunk 00000000-0000-0000-0000-000000000002'
+
+
 echo Sending Traces to:
 set | grep RECORDER_
 
